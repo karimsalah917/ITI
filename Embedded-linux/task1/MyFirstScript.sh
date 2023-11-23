d41179fe89c0af143c68732f3a8806fa151821c5
karim mashreqy <karimsalah917@gmail.com>
2023-11-23 02:36:36 +0200

    adding task 1 first bash script

---
+++ Embedded-linux/task1/MyFirstScript.sh
@@ -0,0 +1,21 @@
+#first bashscript
+
+#!/bin/bash
+
+mkdir ~/mydirectory
+
+mkdir ~/mydirectory/seconddirectory
+
+touch ~/mydirectory/seconddirectory/mynotepaper
+
+cp ~/mydirectory/seconddirectory/mynotepaper ~/mydirectory/
+
+mv ~/mydirectory/mynotepaper ~/mydirectory/myoldnotepaper
+
+echo "your bash is completed!!"
+
+
+
+
+
+
